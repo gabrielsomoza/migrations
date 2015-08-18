c02f484900fa5fc940309375b091d7141b4bc5d9
Gabriel Somoza <gabriel@somoza.me>
2015-08-18 09:58:39 +0200

    Started refactor towards using Baleen/Cli to power Doctrine Migrations.

---
+++ config/defaults.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'migrations' => [
+        'directory' => 'migrations',
+        'namespace' => 'Migrations',
+    ],
+    'storage_file' => '.baleen_versions',
+];
---
+++ migrations/v20150811090345_SomeTitle.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Migrations;
+
+use Baleen\Migrations\Migration\SimpleMigration;
+
+class v20150811090345_SomeTitle extends SimpleMigration
+{
+
+    public function up()
+    {
+        sleep(rand(1,3));
+    }
+
+    public function down()
+    {
+        sleep(rand(1,3));
+    }
+
+
+}
+
